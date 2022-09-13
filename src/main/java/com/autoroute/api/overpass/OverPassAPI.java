0f1c825151d1523c1a3a628cb840df41da30bece
Charm <charmik1994@gmail.com>
2022-09-14 01:41:37 +0300

    add more URLs for OverPassAPI

--- src/main/java/com/autoroute/api/overpass/OverPassAPI.java
+++ src/main/java/com/autoroute/api/overpass/OverPassAPI.java
@@ -2,6 +2,7 @@ package com.autoroute.api.overpass;
 
 import com.autoroute.osm.Tag;
 import de.westnordost.osmapi.OsmConnection;
+import de.westnordost.osmapi.common.errors.OsmConnectionException;
 import de.westnordost.osmapi.map.data.BoundingBox;
 import de.westnordost.osmapi.map.data.LatLon;
 import de.westnordost.osmapi.map.data.Node;
@@ -19,21 +20,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class OverPassAPI {
 
     private static final Logger LOGGER = LogManager.getLogger(OverPassAPI.class);
 
-    private final OverpassMapDataApi overpass;
+    private static final String[] URLS = {
+        "https://overpass-api.de/api/",
+        "https://overpass.kumi.systems/api/",
+        "http://overpass.openstreetmap.ru/cgi/",
+        "https://overpass.openstreetmap.fr/api/"
+    };
 
     public OverPassAPI() {
-        OsmConnection connection = new OsmConnection("https://overpass-api.de/api/", "User-Agent: Mozilla/5.0");
-        connection.setTimeout(30 * 60 * 1000);
-        this.overpass = new OverpassMapDataApi(connection);
+
     }
 
     public List<OverpassResponse> getNodesInBoxByTags(@NotNull Box box, Set<Tag> tags) {
-        StringBuilder query = new StringBuilder("<union>\n");
+        StringBuilder query = new StringBuilder("<osm-script timeout=\"120\" element-limit=\"1073741824\">\n")
+            .append("<union>\n");
         for (Tag tag : tags) {
             query.append("<query type=\"node\">\n");
             query.append("<has-kv k=\"")
@@ -48,6 +54,7 @@ public class OverPassAPI {
         }
         query.append("</union>\n");
         query.append("<print mode=\"meta\"/>\n");
+        query.append("</osm-script>");
 
         LOGGER.info("query:\n" + query);
 
@@ -82,16 +89,31 @@ public class OverPassAPI {
         Repeater.create("Wait to get data by OverPass API")
             .until(() -> {
                 try {
+                    OverpassMapDataApi overpass = createOverpass();
                     overpass.queryElementsWithGeometry(query.toString(), handler);
                     return true;
+                } catch (OsmConnectionException e) {
+                    LOGGER.info("couldn't get data by OverPass API, try to repeat: ", e);
+                    LOGGER.info(e.getMessage() + " " + e.getDescription() + e);
+                    return false;
                 } catch (Exception e) {
-                    LOGGER.info("couldn't get data by OverPass API, try to repeat: " + e.getMessage());
+                    LOGGER.info("couldn't get data by OverPass API, try to repeat: ", e);
                     return false;
                 }
             })
-            .limitIterationsTo(10)
-            .backoff(Duration.FIVE_SECONDS, 5, Duration.FIVE_MINUTES)
+            .limitIterationsTo(20)
+            .backoff(Duration.THIRTY_SECONDS, 2, Duration.FIVE_MINUTES)
             .run();
         return responses;
+    }
+
+    @NotNull
+    private static OverpassMapDataApi createOverpass() {
+        int timeout = 10 * 60 * 1000;
+        String url = URLS[ThreadLocalRandom.current().nextInt(URLS.length)];
+        OsmConnection connection = new OsmConnection(
+            url, "User-Agent: Mozilla/5.0", null, timeout);
+        LOGGER.info("Create overpass with url: {}", url);
+        return new OverpassMapDataApi(connection);
     }
 }
