0ad61685fa1f7f1a9b513d95764e56ffeaa693c2
Charm <charmik1994@gmail.com>
2022-09-16 02:56:12 +0300

    remove IOException in tagsReader

--- src/main/java/com/autoroute/tags/TagsFileReader.java
+++ src/main/java/com/autoroute/tags/TagsFileReader.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class TagsFileReader {
@@ -16,33 +17,35 @@ public class TagsFileReader {
         this.tags = new HashSet<>();
     }
 
-    public void readTags() throws IOException {
+    public void readTags() {
         readTags("cool_tags.txt");
     }
 
-    public void readTags(String fileName) throws IOException {
-        var lines =
-            Files.readAllLines(
+    public void readTags(String fileName) {
+        try {
+            List<String> lines = Files.readAllLines(
                 Paths.get("config")
                     .resolve(Paths.get(fileName)));
-        tags.clear();
-        for (String line : lines) {
-            if (line.startsWith("--")) {
-                continue;
+            tags.clear();
+            for (String line : lines) {
+                if (line.startsWith("--")) {
+                    continue;
+                }
+                final String[] split_line = line.split(" ");
+                if (split_line.length != 2) {
+                    throw new IllegalStateException("tags are not correct: " + line);
+                }
+                final String key = split_line[0].strip();
+                final String value = split_line[1].strip();
+                Tag tag = new Tag(key, value);
+                if (tags.contains(tag)) {
+                    throw new IllegalStateException("tags are not correct, duplicate key: " + key);
+                }
+                tags.add(tag);
             }
-            final String[] split_line = line.split(" ");
-            if (split_line.length != 2) {
-                throw new IllegalStateException("tags are not correct: " + line);
-            }
-            final String key = split_line[0].strip();
-            final String value = split_line[1].strip();
-            Tag tag = new Tag(key, value);
-            if (tags.contains(tag)) {
-                throw new IllegalStateException("tags are not correct, duplicate key: " + key);
-            }
-            tags.add(tag);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
     }
 
     public Set<Tag> getTags() {
