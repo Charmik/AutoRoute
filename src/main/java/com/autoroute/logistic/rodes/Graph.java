a5aa76dedd93c28c70587f630e5f4c7a61d9afb3
Charm <charmik1994@gmail.com>
2022-09-20 23:07:29 +0300

    add Graph class

---
+++ src/main/java/com/autoroute/logistic/rodes/Graph.java
@@ -0,0 +1,10 @@
+package com.autoroute.logistic.rodes;
+
+public class Graph {
+
+    private static class Vertex {
+        int id; // all info about vertex we can get from id
+        int[] neighbors;
+    }
+
+}
