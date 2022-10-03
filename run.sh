3a80e30e02e7a47eddfd2cea162712c58c9a800b
Charm <charmik1994@gmail.com>
2022-10-03 22:58:57 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ set -ex
 
 mvn compile assembly:single
 java \
--ea -Xmx250m -XX:+UseParallelGC -Xlog:gc*:file=logs/gc.log \
+-ea -Xmx300m -XX:+UseSerialGC -Xlog:gc*:file=logs/gc.log \
 -XX:+HeapDumpOnOutOfMemoryError -XX:NativeMemoryTracking=summary -XX:GCTimeLimit=15 \
 -jar target/AutoRoute.jar
