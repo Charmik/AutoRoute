723ece9d661a45dc49efe3bf67eabea837dedbc9
charm <charmik1994@gmail.com>
2022-11-05 19:44:35 +0200

    improve Vertex equals perf

--- src/main/java/com/autoroute/logistic/rodes/Vertex.java
+++ src/main/java/com/autoroute/logistic/rodes/Vertex.java
@@ -101,11 +101,7 @@ public class Vertex {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
         Vertex vertex = (Vertex) o;
-
         return id == vertex.id;
     }
 
